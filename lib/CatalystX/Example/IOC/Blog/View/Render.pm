4e20cfdf28ef826d9f98efcd6bf3fd8f7fe16202
André Walker <andre@andrewalker.net>
2012-10-19 00:48:15 -0300

    Add Xslate view

---
+++ lib/CatalystX/Example/IOC/Blog/View/Render.pm
@@ -0,0 +1,23 @@
+package CatalystX::Example::IOC::Blog::View::Render;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::Xslate';
+
+__PACKAGE__->config(
+    template_extension => '.tx',
+);
+
+1;
+
+=head1 NAME
+
+CatalystX::Example::IOC::Blog::View::Render - Xslate View for CatalystX::Example::IOC::Blog
+
+=head1 DESCRIPTION
+
+Xslate View for CatalystX::Example::IOC::Blog.
+
+=cut
+
